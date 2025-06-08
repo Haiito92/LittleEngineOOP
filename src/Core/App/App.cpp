318af643f0c607697175e8f989af0bace6b83f5d
Haiito92 <hannaantoinebusiness@gmail.com>
2025-06-08 17:12:02 +0200

    Remove logs

--- src/Core/App/App.cpp
+++ src/Core/App/App.cpp
@@ -23,12 +23,10 @@ namespace LittleEngine::Core {
         Time& time = Time::GetInstance();
         time.Start();
 
-        Log::LogInfo("App", "Start App Loop");
         while (appWindow.ProcessMessages()) {
             //App or Game loop
             time.Tick();
             m_game->Update(time.GetDeltaTime());
-            Log::LogInfo("App", "Delta Time: {}", time.GetDeltaTime());
         }
     }
 
