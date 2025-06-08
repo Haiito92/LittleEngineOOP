86717d924723853236f9295cffd9d2d35da474a8
Haiito92 <hannaantoinebusiness@gmail.com>
2025-06-08 16:13:54 +0200

    Fix IGame virtual funcs

--- include/LittleEngine/Core/Game/IGame.h
+++ include/LittleEngine/Core/Game/IGame.h
@@ -11,8 +11,8 @@ namespace LittleEngine::Core {
     public:
         virtual void Init() = 0;
         virtual void Update(float deltaTime) = 0;
-        virtual void Render();
-        virtual void Shutdown();
+        virtual void Render() = 0;
+        virtual void Shutdown() = 0;
         virtual ~IGame() = default;
     };
 
